32475e77902f5c95ae1bb7046f29f1d314f40da6
James Billingham <james@billingham.net>
2014-10-05 06:58:16 -0700

    Better flash notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
 	def render_mac(mac)
 		mac.scan(/.{2}/).join(':')
 	end
+
+	def bootstrap_class_for(flash_type)
+		case flash_type
+			when 'success'
+				'alert-success'
+			when 'error'
+				'alert-error'
+			when 'alert'
+				'alert-block'
+			when 'notice'
+				'alert-info'
+			else
+				flash_type.to_s
+		end
+	end
 end
